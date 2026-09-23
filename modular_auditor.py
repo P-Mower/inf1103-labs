ffe3bf57d81f27ab01d1698ba564adfa1092c705
Priscilla <2603682@sit.singaporetech.edu.sg>
2026-09-23 14:51:03 +0800

    Mod Post

---
+++ modular_auditor.py
@@ -0,0 +1,52 @@
+
+failed_attempts = 0 
+total_units = 0
+current_total = 0
+
+
+
+def get_valid_input():
+    global failed_attempts
+    while True:
+        stock = input("Please enter a stock quantity (or type 'quit' to exit): ")
+        if stock.isdigit():
+            return int(stock)
+        elif stock == "quit":
+            return stock
+        else:
+            failed_attempts += 1
+            print("error")
+        
+def process_delivery(current_total, new_value):
+    current_total = current_total + new_value
+    return current_total
+    
+def calculate_tax(amount):
+    tax = amount * 0.1 
+    return tax
+
+def generate_report(total_units, failed_attempts):
+    print("Total Deliveries Processed:", total_units)
+    print("Number of Failed/Rejected Entries:", failed_attempts)
+    
+while (True): 
+    stock = get_valid_input()
+
+    if stock == "quit":
+        generate_report(total_units, failed_attempts)
+        break
+    else:
+        total_units += 1
+        print ("Total Inventory:", process_delivery(current_total, stock))
+        print("Tax Amount:", calculate_tax(stock))
+        if current_total > 500:
+            print("Alert! Inventory has overstocked.")
+            break
+        
+        
+            
+        
+        
+        
+        
+        
