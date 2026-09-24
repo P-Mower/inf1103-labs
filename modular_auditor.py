ed5be99cecae0b4a0e16af14b14786e4f7fc005c
Priscilla <2603682@sit.singaporetech.edu.sg>
2026-09-24 14:00:33 +0800

    mod post v2

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,28 +1,26 @@
 
-failed_attempts = 0 
+
 total_units = 0
 current_total = 0
-
+failed_attempts = 0
 
 
 def get_valid_input():
-    global failed_attempts
-    while True:
         stock = input("Please enter a stock quantity (or type 'quit' to exit): ")
         if stock.isdigit():
             return int(stock)
         elif stock == "quit":
             return stock
         else:
-            failed_attempts += 1
             print("error")
+            return None
         
 def process_delivery(current_total, new_value):
     current_total = current_total + new_value
     return current_total
     
 def calculate_tax(amount):
-    tax = amount * 0.1 
+    tax = amount*0.1 
     return tax
 
 def generate_report(total_units, failed_attempts):
@@ -32,17 +30,22 @@ def generate_report(total_units, failed_attempts):
 while (True): 
     stock = get_valid_input()
 
+    if stock == None:
+        failed_attempts += 1
+        continue
+    
     if stock == "quit":
-        generate_report(total_units, failed_attempts)
-        break
-    else:
-        total_units += 1
-        print ("Total Inventory:", process_delivery(current_total, stock))
-        print("Tax Amount:", calculate_tax(stock))
-        if current_total > 500:
-            print("Alert! Inventory has overstocked.")
+            generate_report(total_units, failed_attempts)
             break
         
+    else: 
+        total_units += 1
+        current_total = process_delivery(current_total, stock)
+        print("Tax Amount:", calculate_tax(stock))
+        print ("Total Inventory:", current_total)
+        if current_total > 500:
+            print ("Alert! Inventory has overstocked.")
+            break
         
             
         
